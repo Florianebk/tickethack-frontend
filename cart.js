59872597b693af0885198f4287de06b13b07a7f5
Your name <Your email>
2024-11-06 14:55:37 +0100

    désolée pour le _

--- cart.js
+++ cart.js
@@ -1,15 +1,70 @@
 
+function purchaseButton() {
+    document.querySelectorAll('#purchase').forEach(button => {
+        button.addEventListener('click', function () {
+            fetch('http://localhost:3000/basket/update')
+                .then((response => response.json()))
+                .then(data => {
+                    if (data.result) {
+
+                        window.location.assign('booking.html')
+
+                    }
+                })
+
+        })
+    })
+
+}
+
+function updateCount() {
+    fetch('http://localhost:3000/basket/cart')
+        .then(response => response.json())
+        .then(data => {
+
+            let newCount = 0;
+            if (data.cart.length > 0) {
+                for (let el of data.cart) {
+                    newCount += el.price
+
+                }
+            }
+
+        })
+}
+
+function deleteButton() {
+    document.querySelectorAll('.delete').forEach(button => {
+        button.addEventListener('click', function () {
+
+            const getId = this.getAttribute('data-id')
+            console.log(getId)
+
+
+            fetch(`http://localhost:3000/basket/delete/${getId}`, {
+                method: 'DELETE',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.result) {
+                        this.parentNode.remove()
+                        updateCount()
+                    }
+                })
+        })
+    });
+}
 
 fetch('http://localhost:3000/basket/cart')
-.then(response => response.json())
-.then(data => {
-    if(data.cart.length > 0){
+    .then(response => response.json())
+    .then(data => {
+        if (data.cart.length > 0) {
 
-        let count = 0 
- 
-        document.querySelector('.welcome').innerHTML =''
+            let count = 0
 
-        document.querySelector('.welcome').innerHTML = `
+            document.querySelector('.welcome').innerHTML = ''
+
+            document.querySelector('.welcome').innerHTML = `
         <div class="content">
         <div class='line-cart'>
         </div>
@@ -21,68 +76,33 @@ fetch('http://localhost:3000/basket/cart')
         </div>
         </div>`
 
-        for(let el of data.cart){
+            for (let el of data.cart) {
 
-            let newDate = new Date(el.date);
-            let heure = newDate.getHours()
-            let minutes = newDate.getMinutes()
+                let newDate = new Date(el.date);
+                let heure = newDate.getHours()
+                let minutes = newDate.getMinutes()
 
-            count += el.price
+                count += el.price
 
-            document.querySelector('.line-cart').innerHTML += `
+                document.querySelector('.line-cart').innerHTML += `
             <div class='ticket'>
             ${el.departure}>${el.arrival} ${heure}:${String(minutes).padStart(2, '0')} ${el.price}€
-            <button id="delete" data-id='${el.id}'> delete </button>
+            <button class="delete" data-id='${el._id}'> delete </button>
             </div> `
+                document.querySelector('#count').innerHTML = `${count}€`
+
+
+            }
+
             document.querySelector('#count').innerHTML = `${count}€`
 
-            deleteButton()
-            purchaseButton()
 
         }
-
-
-    }
-})
-
-function deleteButton(){
-    document.querySelectorAll('#delete').forEach( button => {
-        button.addEventListener('click', function() {
-
-            const getId = this.getAttribute('data-id')
-                
-                
-                fetch(`http://localhost:3000/basket/delete/${getId}`,{
-                method:'DELETE',
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if(data.result){
-                        this.parentNode.remove()
-                    }      
-                }) 
-        })          
-    }); 
-}
-
-function purchaseButton(){
-    document.querySelectorAll('#purchase').forEach( button => {
-        button.addEventListener('click', function() {
-            fetch('http://localhost:3000/basket/update')
-            .then((response => response.json()))
-            .then(data => {
-                if(data.result){
-
-                     window.location.assign('booking.html')
-
-                }      
-            }) 
-
-        })
+    })
+    .then(data => {
+        deleteButton()
+        purchaseButton()
     })
 
-}
 
-function updateCount(){
 
-}
